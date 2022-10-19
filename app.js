b8e3bb579d758af12b50a084af500377e3d441d8
mahamed81 <mahamed10ahmed@gmail.com>
2022-10-18 23:21:05 -0500

    added passport authen wih sesstion and jwt

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
-var cookieParser = require("cookie-parser");
+const passport = require('passport');
+const authenticate = require('./authenticate');
 var logger = require("morgan");
-const session = require('express-session');
-const FileStore = require('session-file-store')(session);
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
- 
+const config = require('./config');
+const url = config.mongoUrl;
 const campsiteRouter = require("./routes/campsiteRouter");
 const promotionRouter = require("./routes/promotionRouter");
 const partnerRouter = require("./routes/partnerRouter");
 
 // Mongoose connection
 const mongoose = require("mongoose");
-
-const url = "mongodb://localhost:27017/nucampsite";
 const connect = mongoose.connect(url, {
   useCreateIndex: true,
   useFindAndModify: false,
@@ -37,35 +35,11 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}));
+app.use(passport.initialize());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-function auth(req, res, next) {
-    console.log(req.session);
 
-    if (!req.session.user) {
-        const err = new Error('You are not authenticated!');
-        err.status = 401;
-        return next(err);
-    } else {
-        if (req.session.user === 'authenticated') {
-            return next();
-        } else {
-            const err = new Error('You are not authenticated!');
-            err.status = 401;
-            return next(err);
-        }
-    }
-}
-
-app.use(auth);
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/campsites", campsiteRouter);
 app.use("/promotions", promotionRouter);
